91157827554f5bf4adce7a0807098a66d9251630
JordanSteeleGIT <39620858+JordanSteele96@users.noreply.github.com>
2022-07-30 15:57:18 +0100

    Gave each card an img source and alt text

--- src/Components/Products/Product.jsx
+++ src/Components/Products/Product.jsx
@@ -24,7 +24,7 @@ const Product = ({ title, paragraph, buttonData, imgData }) => {
           })}
         </div>
         <div className="card-lower">
-          <img src=""></img>
+          <img src={`Images/${imgData.imgURL}`} alt={imgData.imgAlt} />
         </div>
       </div>
     </div>
