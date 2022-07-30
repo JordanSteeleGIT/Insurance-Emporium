4f656e2ab25be1c88d2a60dca5a4bbd122715d04
JordanSteeleGIT <39620858+JordanSteele96@users.noreply.github.com>
2022-07-30 15:33:26 +0100

    Initial upload with data

---
+++ src/Data/ProductData.jsx
@@ -0,0 +1,187 @@
+export const CARD_DATA = [
+  {
+    title: "Pet Insurance",
+    paragraph:
+      "Pets are much-loved members of the family who bring a lot of magic into our lives. Whether your cat or dog is young or old, we insure them from 5 weeks to any age at The Insurance Emporium!",
+    imgData: {
+      imgURL: "eddie_pet_insurance.png",
+      imgAlt: "Eddie waving",
+    },
+    ctaLinks: [
+      {
+        ctaTitle: "Dog Insurance",
+        ctaButtonLink:
+          "https://www.theinsuranceemporium.co.uk/products/pet/quote",
+        ctaLearnMoreLink:
+          "https://www.theinsuranceemporium.co.uk/products/pet/dog",
+      },
+      {
+        ctaTitle: "Cat Insurance",
+        ctaButtonLink:
+          "https://www.theinsuranceemporium.co.uk/products/pet/quote",
+        ctaLearnMoreLink:
+          "https://www.theinsuranceemporium.co.uk/products/pet/cat",
+      },
+    ],
+  },
+  {
+    title: "Equine Insurance",
+    paragraph:
+      "Insure your horse from 31 days to any age, or choose rider only.",
+    imgData: {
+      imgURL: "eddie_horse_insurance.png",
+      imgAlt: "Eddie with horse",
+    },
+    ctaLinks: [
+      {
+        ctaTitle: "Horse Insurance",
+        ctaButtonLink:
+          "https://www.theinsuranceemporium.co.uk/products/equine/horse/quote",
+        ctaLearnMoreLink:
+          "https://www.theinsuranceemporium.co.uk/products/equine/horse",
+      },
+      {
+        ctaTitle: "Verteran Horse Insurance",
+        ctaButtonLink:
+          "https://www.theinsuranceemporium.co.uk/products/equine/veteran-horse/quote",
+        ctaLearnMoreLink:
+          "https://www.theinsuranceemporium.co.uk/products/equine/veteran-horse",
+      },
+      {
+        ctaTitle: "Horse Rider Insurance",
+        ctaButtonLink:
+          "https://www.theinsuranceemporium.co.uk/products/equine/horse-rider/quote",
+        ctaLearnMoreLink:
+          "https://www.theinsuranceemporium.co.uk/products/equine/horse-rider",
+      },
+    ],
+  },
+  {
+    title: "Caravan Insurance",
+    paragraph:
+      "Whether you tow a caravan or trailer tent, or stay in a static, you can pitch up with one of our policy types then tweak it with a range of Optional Benefits to make it all yours!",
+    imgData: {
+      imgURL: "eddie_caravan_insurance.png",
+      imgAlt:
+        "Eddie trying to control a flaming barbecue behind a touring caravan",
+    },
+    ctaLinks: [
+      {
+        ctaTitle: "Touring Caravan Insurance",
+        ctaButtonLink:
+          "https://www.theinsuranceemporium.co.uk/products/caravan/touring-caravan/quote",
+        ctaLearnMoreLink:
+          "https://www.theinsuranceemporium.co.uk/products/caravan/touring-caravan",
+      },
+      {
+        ctaTitle: "Static Caravan Insurance",
+        ctaButtonLink:
+          "https://www.theinsuranceemporium.co.uk/products/caravan/static-caravan/quote",
+        ctaLearnMoreLink:
+          "https://www.theinsuranceemporium.co.uk/products/caravan/static-caravan",
+      },
+    ],
+  },
+  {
+    title: "Wedding Insurance",
+    paragraph:
+      "Getting married at home or away? Whatever you’re planning for your big day, why not waltz down the aisle knowing your day is protected?",
+    imgData: {
+      imgURL: "eddie_wedding_insurance.png",
+      imgAlt: "Eddie struggling not to drop a big wedding cake",
+    },
+    ctaLinks: [
+      {
+        ctaTitle: "UK Wedding Insurance",
+        ctaButtonLink: null,
+        ctaLearnMoreLink:
+          "https://www.theinsuranceemporium.co.uk/products/wedding/uk-wedding",
+      },
+      {
+        ctaTitle: "Overseas Wedding Insurance",
+        ctaButtonLink: null,
+        ctaLearnMoreLink:
+          "https://www.theinsuranceemporium.co.uk/products/wedding/overseas-wedding",
+      },
+    ],
+  },
+  {
+    title: "Camera Insurance",
+    paragraph:
+      "Whether you’re a photographer or videographer, professional or enthusiast, we offer tweakable insurance for cameras, telescopes and many more bits of related equipment.",
+    imgData: {
+      imgURL: "eddie_camera_insurance.png",
+      imgAlt: "Eddie with his camera",
+    },
+    ctaLinks: [
+      {
+        ctaTitle: "Camera Insurance - Amateur",
+        ctaButtonLink:
+          "https://www.theinsuranceemporium.co.uk/products/camera/quote",
+        ctaLearnMoreLink:
+          "https://www.theinsuranceemporium.co.uk/products/camera",
+      },
+      {
+        ctaTitle: "Camera Insurance - Pro",
+        ctaButtonLink:
+          "https://www.theinsuranceemporium.co.uk/products/camera/quote",
+        ctaLearnMoreLink:
+          "https://www.theinsuranceemporium.co.uk/products/camera",
+      },
+    ],
+  },
+  {
+    title: "Cycle Insurance",
+    paragraph:
+      "Your bike can take you on many magical adventures! Our Pick ‘N’ Mix bicycle insurance can be tailored to suit all sorts of riders, whether you race on roads or simply cycle to work. Having cycle insurance could give you peace of mind and help keep your wheels turning. Find out more to put the brakes on some of the worries you might have.",
+    imgData: {
+      imgURL: "eddie_cycle_insurance.png",
+      imgAlt: "Eddie with his bicycle",
+    },
+    ctaLinks: [
+      {
+        ctaTitle: null,
+        ctaButtonLink:
+          "https://www.theinsuranceemporium.co.uk/products/cycle/quote",
+        ctaLearnMoreLink:
+          "https://www.theinsuranceemporium.co.uk/products/cycle",
+      },
+    ],
+  },
+  {
+    title: "Fishing Insurance",
+    paragraph:
+      "Packing up your rod and heading for an angling adventure? Making sure that you have some Fishing Insurance could be vital in helping you protect your gear if something goes awry. Choose from one of our three fully Tweakable policies, with a choice of Optional Benefits to suit your needs. Don’t get caught out, find more ‘reely’ useful information!",
+    imgData: {
+      imgURL: "eddie_fishing_insurance.png",
+      imgAlt: "Eddie is happy as he has caught a huge yellow fish",
+    },
+    ctaLinks: [
+      {
+        ctaTitle: null,
+        ctaButtonLink:
+          "https://www.theinsuranceemporium.co.uk/products/fishing/quote",
+        ctaLearnMoreLink:
+          "https://www.theinsuranceemporium.co.uk/products/fishing",
+      },
+    ],
+  },
+  {
+    title: "Golf Insurance",
+    paragraph:
+      "No matter what your handicap level is, whether you’ve scored a hole-in-one and need to buy a round or your equipment has been accidentally damaged, our Golf Insurance might help to protect you! Our different levels of Tweakable golf equipment insurance could suit a variety of budgets. Click the buttons below to get a quote or find out more!",
+    imgData: {
+      imgURL: "eddie_golf_insurance.png",
+      imgAlt: "Eddie with his golf clubs",
+    },
+    ctaLinks: [
+      {
+        ctaTitle: null,
+        ctaButtonLink:
+          "https://www.theinsuranceemporium.co.uk/products/golf/quote",
+        ctaLearnMoreLink:
+          "https://www.theinsuranceemporium.co.uk/products/golf",
+      },
+    ],
+  },
+];
